d428b80c20417f4474ef3e96c14394e4d766b671
xinfan03 <100601043+xinfan03@users.noreply.github.com>
2022-04-19 20:46:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include<conio.h>
+#include<dos.h>
+#include <time.h>
+//4 thư viện chính cơ bản
+#include <windows.h>
+#include<string>
+#include<SDL.h>
+#include<SDL_image.h>
 
 using namespace std;
 
